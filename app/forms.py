5fc258b326e60c2b2c43cb4a208c04c093264e61
NarayanAdithya <narayanadithya1234@gmail.com>
2020-11-20 22:51:17 +0530

    Added User Model, and routes

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,34 @@
 #Web Forms for Registration and Signin Here
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField,SubmitField,RadioField,SelectField,TextAreaField
+from wtforms.fields.html5 import EmailField,URLField,DateField
+from wtforms.validators import DataRequired,Email,EqualTo,ValidationError,URL,Length
+from app.models import User,Courses
+from flask_login import current_user,login_user,logout_user,login_required
+class RegisterForm(FlaskForm):
+    username=StringField('Username',validators=[DataRequired()],render_kw={'class':'form-control form-group'})
+    email=EmailField('Email',validators=[DataRequired(),Email()],render_kw={'class':'form-control form-group'})
+    password=PasswordField('Password',validators=[DataRequired()],render_kw={'class':'form-control form-group'})
+    conpassword=PasswordField('Re-Enter Password',validators=[DataRequired(),EqualTo('password')],render_kw={'class':'form-control form-group'})#('values','label')
+    user_role=RadioField('User Role',validators=[DataRequired()],choices=[('Student','Student'),('Instructor','Instructor')],render_kw={'class':'form-check form-check-input','style':'list-style:none;'})
+    Region=SelectField('Select Region',validators=[(DataRequired())],choices=[('America','America'),('Africa','Africa'),('Asia','Asia'),('Europe','Europe')],render_kw={'class':'form-group col-md-4 form-control '})
+    # remember_me=BooleanField('Keep Me Signed In')
+    submit=SubmitField('Sign In',render_kw={'class':'btn btn-primary btn-pill','style':'height : 50px;'})
+    def validate_username(self,username):
+        user=User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please Use a Different Username.')
+    def validate_email(self,email):
+        user=User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please Use A different Email Address')
+        
+class LoginForm(FlaskForm):
+    email=EmailField('Email',validators=[DataRequired(),Email()],render_kw={'class':'form-control form-group'})
+    password=PasswordField('Password',validators=[DataRequired()],render_kw={'class':'form-control form-group'})
+    remember_me=BooleanField('Keep Me Signed In')
+    submit=SubmitField('Sign In',render_kw={'class':'btn btn-primary btn-pill','style':'height : 50px;'})
+
+
+
+
--- app/routes.py
+++ app/routes.py
@@ -12,11 +12,59 @@ import pickle
 from flask_mail import Message
 from threading import Thread
 
+#Login Part
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form=LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid Email or Password',category="danger")
+            return redirect(url_for('login'))
+        login_user(user,remember=form.remember_me.data)
+        next_page=request.args.get('next')
+        if not next_page or url_parse(next_page).netloc!='':
+            next_page=url_for('index')
+        return redirect(next_page)
+    return render_template('signinpage.html',title='SignIn',form=form)
+
+#Registration
+@app.route('/register',methods=['POST','GET'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form=forms.RegisterForm()
+    if form.validate_on_submit():
+        user=User(username=form.username.data,email=form.email.data,user_role=form.user_role.data,Region=form.Region.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Successfully Registered',category="success")
+        msg = Message('Welcome to CELIS',
+                  sender="celis.students@gmail.com",
+                  recipients=[user.email])
+        msg.body="Hey There, We are happy that you have decided to join our community, We look forward to working with you. If you have any issues do notify us in our contact us section"
+        Thread(target=send_async_email,args=(app,msg)).start()
+        print(form.password.data)
+        print(form.user_role.data)
+        print(form.Region.data)
+        return redirect(url_for('login'))
+    return render_template('signuppage.html',form=form,title='Register')
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('celis.html',title='Home',data_footer_aos="fade-left",data_aos_footer_delay=100,data_aos_header="fade-left",data_header_aos_delay=100)
+
+
+@app.route('/logout')
+def logout():
+    current_user.last_seen=datetime.utcnow()
+    db.session.commit()
+    logout_user()
+    return redirect(url_for('index'))
 
 
