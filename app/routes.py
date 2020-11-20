89260769b70c0f8af02ed77ee8bf5257c451cbb0
NarayanAdithya <narayanadithya1234@gmail.com>
2020-11-20 22:34:46 +0530

    Added Route for Homepage

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,22 @@
 #Code for all Linked Routes
+from app import app,forms,db,socketio,mail
+from flask_socketio import emit,leave_room,join_room
+from flask import request,redirect,url_for,render_template,flash,get_flashed_messages,flash,jsonify
+from flask_login import current_user,login_user,logout_user,login_required
+from app.models import User,thread,post,Courses,enrolled
+from app.forms import LoginForm,RegisterForm,add_course_form,RequestResetForm, ResetPasswordForm
+from werkzeug.urls import url_parse
+from wtforms.validators import ValidationError
+from datetime import datetime
+import pickle
+from flask_mail import Message
+from threading import Thread
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('celis.html',title='Home',data_footer_aos="fade-left",data_aos_footer_delay=100,data_aos_header="fade-left",data_header_aos_delay=100)
+
+
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir=os.path.abspath(os.path.dirname(__file__))
 
 class config(object):
-    SECRET_KEY= os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY= os.environ.get('SECRET_KEY') or '334nadnj&&89Ydau89YAd98adbszmdi3*&&923kln'
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or \
         'sqlite:///'+os.path.join(basedir,'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
@@ -11,5 +11,5 @@ class config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['narayanadithya1234@gmail.com','aravindharinarayanan111@gmail.com']
+    ADMINS = ['narayanadithya1234@gmail.com','aravindharinarayanan111@gmail.com','sonusvareed@gmail.com']
 
