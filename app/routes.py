22d3923a2a7570f6a9199a3f023a994170cb9790
NarayanAdithya <narayanadithya1234@gmail.com>
2020-11-21 17:25:19 +0530

    Added View Pages and ForumHome Page

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,3 @@
-#Code for all Linked Routes
 from app import app,forms,db,socketio,mail
 from flask_socketio import emit,leave_room,join_room
 from flask import request,redirect,url_for,render_template,flash,get_flashed_messages,flash,jsonify
@@ -299,3 +298,35 @@ def reset_token(token):
         flash('Your password has been updated! You are now able to log in', 'success')
         return redirect(url_for('login'))
     return render_template('reset_token.html', title='Reset Password', form=form)
+
+
+#socket events
+
+@socketio.on('join')
+def join_room_(data):
+    join_room(data['room'])
+    socketio.emit('status',data,room=data['room'],dif_user=0)
+
+@socketio.on('leave')
+def leave_room_(data):
+    leave_room(data['room'])
+    print('User gonna leave')
+    socketio.emit('left_room_announcement',data,room=data['room'],dif_user=0)
+
+@socketio.on('send_message')
+def send_message(data):
+    user_=User.query.filter_by(username=data['username']).first()
+    thread_=thread.query.filter_by(subject=data['room']).first()
+    p=post(message=data['message'],user_id=user_.id,thread_id=thread_.id)
+    db.session.add(p)
+    db.session.commit()
+    p=post.query.filter_by(message=data['message'],user_id=user_.id,thread_id=thread_.id).first()
+    socketio.emit('received_message',{'room':data['room'],'user_id':p.user_id,'username':user_.username,'msg':p.message,'post_id':p.id,'thread_id':thread_.id},room=data['room'],dif_user=p.user_id)
+
+@socketio.on('remove')
+def remove_post(data):
+    id=int(data['post_id'].split('f')[1])
+    post_=post.query.filter_by(id=id).first()
+    db.session.delete(post_)
+    db.session.commit()
+    socketio.emit('confirm_remove',{"id":data['post_id']},room=data['room'])
